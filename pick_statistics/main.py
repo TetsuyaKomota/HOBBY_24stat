28d6c3ee79def6fe2ae5d1727d5cef30c9e28338
komot <komotaymkty@gmail.com>
2021-12-05 18:19:02 +0900

    [fix]

--- pick_statistics/main.py
+++ pick_statistics/main.py
@@ -147,7 +147,7 @@ def main(event, context):
     print(pubsub_message)
     data               = pubsub_message.split("-")
     begin_idx          = int(data[0])
-    end_idx            = int(data[1]) if len(end_idx) > 0 else None
+    end_idx            = int(data[1]) if len(data) >= 1 and len(data[1]) > 0 else None
     for_local          = (len(data) >= 2 and data[2]=="local")
 
     # API KEY や対象のチャンネルIDリストを取得
